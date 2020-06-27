18ba438b8a6481deda9f0171c79df4fd3af7cc3b
saif-md <nusratnajnin2019@gmail.com>
2020-06-27 15:52:32 +0600

    PyVersion Stable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from browser import document, alert
+from browser import document, alert, window
 
 def linkGenerator(event):
   readLink = document['readLink'].value
@@ -9,3 +9,10 @@ def linkGenerator(event):
   	document['downloadLink'].text = "https://drive.google.com/uc?export=download&id=" + readLink[(readLink.find('/d/')+3):readLink.find('/view')]
 
 document['submitButton'].bind('click', linkGenerator)
+
+def copy(event):
+  textElement = document['downloadLink']
+  textElement.select()
+  document.execCommand('copy')
+  
+document['copyText'].bind('click', copy)
