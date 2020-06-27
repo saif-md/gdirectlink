53b863d42f7d97e13261e210e64118defc5c78ba
saif-md <nusratnajnin2019@gmail.com>
2020-06-27 12:55:05 +0600

    PyVersion beta

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def linkGenerator(event):
   if readLink.find('/d/')==-1:
   	document['downloadLink'].text = "Error: Invalid URL"
   else:
-  	document['downLink'].text = "https://drive.google.com/uc?export=download&id=" + readLink[(readLink.find('/d/')+3):readLink.find('/view')]
+  	document['downloadLink'].text = "https://drive.google.com/uc?export=download&id=" + readLink[(readLink.find('/d/')+3):readLink.find('/view')]
 
-document['submitButton'].bind('click', downloadLink)
+document['submitButton'].bind('click', linkGenerator)
